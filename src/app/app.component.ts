34ce54ec0d724721e99a2e306cc457dd6e8011b6
Michel Galan <michelgalan90@gmail.com>
2021-04-14 09:52:01 -0400

    juego de escritura using bulma.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  onInput(value: string) {
+
+    console.log(value);
+
+    this.enteredText = value;
+
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (enteredLetter) {
+      if (randomLetter === enteredLetter) {
+        return 'green';
+      } else return 'red';
+    } else return 'pending'
+  }
 }
